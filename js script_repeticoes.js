f8d1a114f4142c91c9c2d02cd988fed489e3a431
cristian <cristian.h.santos6@aluno.senai.br>
2026-06-17 15:48:20 -0300

    adicionar

--- js script_repeticoes.js
+++ js script_repeticoes.js
@@ -1,36 +1,44 @@
-//ESTRUTURA DE REPETIÇÃO FOR
-  const divfor = document.querySelector('#div-for')
+//ESTRUTURA DE REPETIÇÃO FOR 
+const divfor = document.querySelector('#div-for')
 
-  for(i = 0;i < 10; i++){
-    console.log(i, 'Amor') 
-    divfor.innerHTML += `${i} - Amor <br>`
-  }
+for(i = 0;i < 10; i++){
+  console.log(i, 'Amor')
+  divfor.innerHTML += `${i}- Amor <br>`
+}
+//CONTADOR/ACUMULADOR
+const inputNum = document.querySelector('#num')
+const btnNum = document.querySelector('#num')
+const divContAcum = document.querySelector('#div-cont-acum')
 
-  //CONTADOR/ACUMULADOR
-  const inputNum = document.querySelector('#num')
-  const btNum = document.querySelector('#num')
-  const divContAcum = document.querySelector('#div-cont-acum')
+let cont = 0
+let acum = 0.0
 
-  let cont = 0
-  let acum = 0.0
+btnNum.addEventListener('click', (evt)=>{
+  let numDigitado = Number(inputNum.value)
 
-  btnNUm.addEventlistener('click', (evt)=>{
-    let numDigitado = Number(inputNum.value)
+  cont++
+  acum += numDigitado
 
-    cont++
-    acum += numDigitado
+  divContAcum.innerHTML = `TOTAL DE NÚMERO DIGITADO É: ${cont} <br>
+  A SOMA DOS NÚMEROS É: ${acum}`
 
-    divContAcum.innerHTML = `TOTAL DE NÚMERO DIGITADO É: ${cont} <br>
-    A SOMA DOS NÚMEROS É:{acum}`
-
-    inputNum.valeu = ''
-    
-  })
-
-  //CONTROLANDO FOR
-  const inputFrase = document.querySelector('#frase')
-  const inputNumRepeticao = document.querySelector('#num-repeticao')
-  const btnFrase = document.querySelector('#btn-frase')
-  const divFrase = document.querySelector('#div-frase')
-
+  inputNum.value = ''
   
+})
+
+//CONTROLANDO FOR
+const inputFrase = document.querySelector('#frase')
+const inputNumRepeticao = document.querySelector('#num-repeticao')
+const btnFrase = document.querySelector('#btn-frase')
+const divFrase = document.querySelector('#div-frase')
+
+btnFrase.addEventListener('click', (evt) => {
+  let numRepeti = Number(inputNumRepeticao.value)
+  let frase = inputFrase.value
+
+  for (i = 0; i < numRepeti; i++){
+    divFrase.innerHTML += `${i + 1} - ${frase} <br>`
+  }
+})
+
+

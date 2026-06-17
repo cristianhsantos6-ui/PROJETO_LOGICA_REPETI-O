aded914e32958189e3433006a031b920d5e3a9b0
cristian <cristian.h.santos6@aluno.senai.br>
2026-06-17 13:46:25 -0300

    adicinar novo

--- js script_repeticoes.js
+++ js script_repeticoes.js
@@ -2,5 +2,35 @@
   const divfor = document.querySelector('#div-for')
 
   for(i = 0;i < 10; i++){
-    console.log(i, 'Amor')
+    console.log(i, 'Amor') 
+    divfor.innerHTML += `${i} - Amor <br>`
   }
+
+  //CONTADOR/ACUMULADOR
+  const inputNum = document.querySelector('#num')
+  const btNum = document.querySelector('#num')
+  const divContAcum = document.querySelector('#div-cont-acum')
+
+  let cont = 0
+  let acum = 0.0
+
+  btnNUm.addEventlistener('click', (evt)=>{
+    let numDigitado = Number(inputNum.value)
+
+    cont++
+    acum += numDigitado
+
+    divContAcum.innerHTML = `TOTAL DE NÚMERO DIGITADO É: ${cont} <br>
+    A SOMA DOS NÚMEROS É:{acum}`
+
+    inputNum.valeu = ''
+    
+  })
+
+  //CONTROLANDO FOR
+  const inputFrase = document.querySelector('#frase')
+  const inputNumRepeticao = document.querySelector('#num-repeticao')
+  const btnFrase = document.querySelector('#btn-frase')
+  const divFrase = document.querySelector('#div-frase')
+
+  

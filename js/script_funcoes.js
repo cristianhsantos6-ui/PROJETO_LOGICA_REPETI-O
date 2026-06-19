cff6a061f0b86ced7d9b5d4fd8a775faf4c5d293
cristian <cristian.h.santos6@aluno.senai.br>
2026-06-19 14:07:05 -0300

    atualizar

---
+++ js/script_funcoes.js
@@ -0,0 +1,26 @@
+
+function mensagem(){
+    console.log('Uma mensagem')
+}
+
+mensagem()
+
+function mensagemLinda(txt){
+    console.log(txt)
+}
+
+mensagemLinda('Brasil vai dar um pau no Haiti')
+
+function somaNumeros(num1, num2){
+    return num1 + num2
+}
+
+console.log('As soma dos números',somaNumeros(10, 58))
+
+const multiplicacao = function(num1, num2){
+    return num1 * num2
+}
+
+console.log(multiplicacao(64, 5))
+
+console.log('-------> ARROW FUNCTION <-----')

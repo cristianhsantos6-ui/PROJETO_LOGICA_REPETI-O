2667fc19fdfc4d8cf58d4499bf3fc72021ed9f01
cristian <cristian.h.santos6@aluno.senai.br>
2026-06-19 15:47:34 -0300

    adicionar

--- js/script_funcoes.js
+++ js/script_funcoes.js
@@ -24,3 +24,79 @@ const multiplicacao = function(num1, num2){
 console.log(multiplicacao(64, 5))
 
 console.log('-------> ARROW FUNCTION <-----')
+
+const msgArrow = () => {
+    console.log('Outra mensagem na arrow function')
+}
+
+msgArrow()
+
+const divisaoNum = (valor1, valor2) => {
+
+    let resposta
+
+    if ((valor1 > 0) && (valor2 > 0)){
+        resposta = valor1 / valor2
+    }else{
+         resposta = 'NÃO É POSSIVEL DIVISÃO POR ZERO'
+    }
+
+    return resposta
+}
+
+console.log(divisaoNum(10,8000))
+
+console.log(Math.random())
+
+console.log('O número sorteado =====> ',parseInt(Math.random() * 100))
+
+setTimeout(()=>{
+    console.log('Executou após 5s')
+    msgArrow()
+}, 5000)
+/*
+let cont = 0
+const intervalo = setInterval(()=>{
+    cont++
+    console.log(cont, 'Retição da setInterval')
+
+    if(cont == 5){
+          clearInterval(intervalo)
+    }
+    
+}, 3000)*/
+
+
+const divNumSorteio = document.querySelector(' #div-num-sorteio')
+
+let contVolta = 0, numSorteado = 0, contPar = 0, contImpar = 0
+
+const sorteo = setInterval(() => {
+    contVolta++
+
+    numSorteado = parseInt(Math.random() * 60)
+
+   console.log('${contVolta}º número sorteado ${numSorteado}')
+
+    if (numSorteado % 2 == 0) {
+      contPar++
+    }else{
+        contImpar++
+    }
+
+    divNumSorteio.innerHTML += `${contVolta}º número gerado ${numSorteado} - ${numSorteado % 2 == 0 ? 'PAR':
+'IMPAR'} <br>`
+
+if (numSorteado == 6) {
+    console.log('-------> SORTEIO ENCERRADO <-----')
+
+    divNumSorteio.innerHTML += `-------> SORTEIO ENCERRADO <----- <br>
+    TOTAL DE NÚMEROS GERADOS: ${contVolta} <br>
+    TOTAL DE NÚMEROS PAR: ${contPar} <br>
+    TOTAL DE NÚMEROS ÍMPAR: ${contImpar}
+    `
+    clearInterval(sorteio)
+    contVolta = 0
+    numSorteado = 0
+}
+}, 2000)
